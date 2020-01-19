102826c7671e2fedd95f34feb00033bd3f048916
黄定和 <huangdinghe@hotmail.com>
2020-01-19 11:13:53 +0800

    网关读取consul内容，轮询方式

--- MicroService.Gateway/Startup.cs
+++ MicroService.Gateway/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
 
 namespace MicroService.Gateway
 {
@@ -29,7 +30,7 @@ namespace MicroService.Gateway
         {
             services.AddControllers();
             //Ìí¼ÓOcelot·þÎñ
-            services.AddOcelot();
+            services.AddOcelot().AddConsul();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
